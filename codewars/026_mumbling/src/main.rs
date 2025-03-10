4b639e7702de6d5c5891d78b368b28df886bd8db
Steven Hall <Hallzy.18@gmail.com>
2025-03-09 23:57:32 -0700

    Mumbling

---
+++ codewars/026_mumbling/src/main.rs
@@ -0,0 +1,17 @@
+fn accum(s:&str)->String {
+    return s
+        .chars()
+        .enumerate()
+        .map(|(i, c)| c.to_string().to_uppercase() + c.to_string().to_lowercase().repeat(i).as_str())
+        .collect::<Vec<String>>()
+        .join("-")
+    ;
+}
+
+fn main() {
+    assert_eq!(accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+    assert_eq!(accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+    assert_eq!(accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+    assert_eq!(accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+    assert_eq!(accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+}
